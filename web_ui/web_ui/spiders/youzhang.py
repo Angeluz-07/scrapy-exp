2b4ca6358301dd93a95bc0aee8ccf052902e5755
roberto <alexandro.ramr777@gmail.com>
2020-01-10 18:21:42 -0500

    UPDATE: modify youzhang spider to scrap only urls

--- web_ui/web_ui/spiders/youzhang.py
+++ web_ui/web_ui/spiders/youzhang.py
@@ -24,6 +24,10 @@ class YouzhangSpider(scrapy.Spider):
     def parse(self, response):
         links_xpath = "//div[@class='col-md-6 mb-8']/div/a/@href"
         for link in response.xpath(links_xpath):
+            yield {
+                'url' : link.get()
+            }
+            """
             yield SplashRequest(
                 url =link.get(), 
                 callback = self.save_screenshot, 
@@ -32,6 +36,7 @@ class YouzhangSpider(scrapy.Spider):
                     'lua_source' : self.lua_script
                 }
             )
+            """
         
         next_page_xpath = "//li[@class='page-item  active ']/following-sibling::li[1]/a/@href"
         next_page = response.xpath(next_page_xpath).get()
