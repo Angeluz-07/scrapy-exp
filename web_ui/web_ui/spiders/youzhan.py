863df03bf8aa9bc4a1aa157420b79d1bbcf3062d
roberto <alexandro.ramr777@gmail.com>
2020-04-18 01:32:48 -0500

    clean web_ui spiders

--- web_ui/web_ui/spiders/youzhan.py
+++ web_ui/web_ui/spiders/youzhan.py
@@ -1,23 +1,24 @@
-# -*- coding: utf-8 -*-
 import scrapy
 
 
-class SiteInspireSpider(scrapy.Spider):
+class YouzhanSpider(scrapy.Spider):
     """
     Scrapes the url of each website listed in the
-    'siteinspire.com/websites' website.
+    'youzhan.org' website.
     """
-    name = 'siteinspire'
-    start_urls = ['http://siteinspire.com/websites/']
+    name = "youzhan"
+    start_urls = [
+        'https://www.youzhan.org/',
+    ]
 
     def parse(self, response):
-        links_xpath = "//div[@class='overlay']/a/@href"
+        links_xpath = "//div[@class='col-md-6 mb-8']/div/a/@href"
         for link in response.xpath(links_xpath):
             yield {
                 'url': link.get()
             }
 
-        next_page_xpath = "//div[@class='pagination pagination-centered']/ul/li[@class='active']/following-sibling::li[1]/a/@href"
+        next_page_xpath = "//li[@class='page-item  active ']/following-sibling::li[1]/a/@href"
         next_page = response.xpath(next_page_xpath).get()
         if next_page:
             next_page = response.urljoin(next_page)
